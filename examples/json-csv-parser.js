e10ffa6b2e2878b73e0545a2d6279c25e2c35002
yogeshwar607 <funnel@Yogis.local>
2021-05-16 04:59:42 +0530

    change to cjs version

--- examples/json-csv-parser.js
+++ examples/json-csv-parser.js
@@ -1,22 +1,25 @@
-import { jsonToCsv } from '../src/lib/parser.mjs'
+const { jsonToCsv } = require('../src/index')
 
 try {
 
   (async function () {
 
-    // output as string 
-    const string = await jsonToCsv({ path: './samples/sample.json', delimiter: ',', quoteChar: '"', newline: "\n", outputMode: 'string', headers: true })
+    //  output as string 
+    const string = await jsonToCsv({ path: '../samples/sample.json', delimiter: ',', quoteChar: '"', newline: "\n", outputMode: 'string', headers: true })
+
+    console.log(string)
+
 
     // output as streams
-    const streamForAwait = await jsonToCsv({ path: './samples/sample.json', delimiter: ',', quoteChar: '"', newline: "\n", outputMode: 'stream', headers: true })
+    const streamForAwait = await jsonToCsv({ path: '../samples/sample.json', delimiter: ',', quoteChar: '"', newline: "\n", outputMode: 'stream', headers: true })
 
     // comsuming using for await 
     for await (const line of streamForAwait) {
       console.log("\n", line);
     }
 
-    // consuming using streams
-    const stream = await jsonToCsv({ path: './samples/sample.json', delimiter: ',', quoteChar: '"', newline: "\n", outputMode: 'stream', headers: true })
+    //consuming using streams
+    const stream = await jsonToCsv({ path: '../samples/sample.json', delimiter: ',', quoteChar: '"', newline: "\n", outputMode: 'stream', headers: true })
 
     stream.on('data', (data) => {
       console.log(data)
